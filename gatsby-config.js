777ef8a3c94fd063002cc6dfc780bdfcd91e3ba2
Naoya Akimoto <akitkat.cook.dx@gmail.com>
2021-07-16 14:44:54 +0900

    edit adsense script

--- gatsby-config.js
+++ gatsby-config.js
@@ -155,6 +155,12 @@ module.exports = {
         name: `images`,
         path: './src/images/',
       },
+    },
+    {
+      resolve: `@isamrish/gatsby-plugin-google-adsense`,
+      options: {
+        googleAdClientId: process.env.GOOGLE_ADSENSE,
+      }
     }
   ],
 }
