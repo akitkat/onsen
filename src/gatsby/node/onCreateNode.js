e9fda8df6a5e1f9ecb6df49b97d6a3543292424d
Naoya Akimoto <akitkat.cook.dx@gmail.com>
2021-07-31 15:44:00 +0900

    :bug: create node.

--- src/gatsby/node/onCreateNode.js
+++ src/gatsby/node/onCreateNode.js
@@ -13,15 +13,14 @@ module.exports = async ({ actions: {
   if (node.internal.type !== `MarkdownRemark`) return
 
   const regex = /^<hotel-info-item.*image="(.[^"]+)".*><\/hotel-info-item>$/gm
-  const tags = getNodesByType('MarkdownRemark')
-  .map(e => e.rawMarkdownBody.match(regex))
-  .filter(e => !!e)
-  .flat()
+  const tags = (node.internal.content.match(regex) ?? [])
+    .filter(e => !!e)
+    .flat()
 
-  if (! tags) return
+    if (! tags) return
 
-  const images = tags.map(e => e.match(/image="([^"]*)"/)[1])
-  if (! images) return
+    const images = tags.map(e => e.match(/image="([^"]*)"/)[1])
+    if (images.length < 1) return
 
   await Promise.all(
     images.map(async url => {
