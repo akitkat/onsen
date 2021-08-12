43f858f0e6e972e9c0c57f9ab0f1a1dbe1eeba6c
Naoya Akimoto <akitkat.cook.dx@gmail.com>
2021-08-12 11:57:32 +0900

    edit build cmd

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 # hotels.json
 wget "https://script.google.com/macros/s/AKfycbxOMhfBA_4ii0NakiM1Zt0N2VuJG9gbioVZoBDTIUYMh2eUPjEh9KVB0LUW5j4gt_MzXg/exec" -O 'hotels.json'
-mv hotels.json static/data/
+mv -v hotels.json static/data/
 
 # cache clear
 gatsby clean
