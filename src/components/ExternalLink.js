12e3ebbf463fba6e31fa475e0157e3846601e62e
Naoya Akimoto <akitkat.cook.dx@gmail.com>
2021-08-16 10:06:30 +0900

    fix class name

--- src/components/ExternalLink.js
+++ src/components/ExternalLink.js
@@ -3,27 +3,27 @@ import styled from '@emotion/styled'
 import { trackCustomEvent } from 'gatsby-plugin-google-analytics'
 
 const Link = styled.a`
-  text-decoration:none;
+  text-decoration: none;
 `
-const ExternalLink = props => {
+const ExternalLink = (props) => {
   return (
     <Link
-      class="external-link"
+      className="external-link"
       href={props.href}
       rel="noopener noreferrer nofollow"
       target="_blank"
       data-vars-event-label={props.ga.eventLabel}
-      onClick={e => {
+      onClick={(e) => {
         trackCustomEvent({
-          category: "External Link",
-          action: "Click",
+          category: 'External Link',
+          action: 'Click',
           label: props.ga.eventLabel ?? '',
         })
       }}
     >
       {props.children}
     </Link>
-  )
+  );
 }
 
 export default ExternalLink
